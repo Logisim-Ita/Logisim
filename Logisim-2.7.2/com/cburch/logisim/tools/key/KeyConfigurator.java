5b200c1304fbb14177010214f49dab7661de5abb
LogisimIt <logisimit@gmail.com>
2017-05-20 19:37:05 +0200

    Removed 2.7.2 version, too unstable

--- Logisim-2.7.2/com/cburch/logisim/tools/key/KeyConfigurator.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2010, Carl Burch. License information is located in the
- * com.cburch.logisim.Main source code and at www.cburch.com/logisim/. */
-
-package com.cburch.logisim.tools.key;
-
-public interface KeyConfigurator {
-	public KeyConfigurator clone();
-	public KeyConfigurationResult keyEventReceived(KeyConfigurationEvent event);
-}
