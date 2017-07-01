258e6052c7566a8920494d0ddafd23bcbaedf971
LogisimIt <logisimit@gmail.com>
2017-07-01 12:08:30 +0200

    Updated translations

--- Logisim-Fork/com/cburch/logisim/Main.java
+++ Logisim-Fork/com/cburch/logisim/Main.java
@@ -30,7 +30,7 @@ import javax.swing.JOptionPane;
 import com.cburch.logisim.gui.start.Startup;
 
 public class Main {
-	public static final LogisimVersion VERSION = LogisimVersion.get(2, 8, 1, 0, "jar");
+	public static final LogisimVersion VERSION = LogisimVersion.get(2, 8, 1, 1, "jar");
 
 	public static final String VERSION_NAME = VERSION.toString();
 
